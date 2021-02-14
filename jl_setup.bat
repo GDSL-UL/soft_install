031be0f1660a0bb0672be0d55fa8d6d25c715e31
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2021-02-14 22:02:38 +0000

    Pin download of python .yml and .txt install files to static version

--- jl_setup.bat
+++ jl_setup.bat
@@ -1,6 +1,6 @@
 REM Run this once the gds environment is created and activated
 REM Add pip packages
-pip install -r https://github.com/darribas/gds_env/raw/master/gds_py/gds_py_pip.txt
+pip install -r https://github.com/darribas/gds_env/raw/7a6a2b3b47d6faa96e854e35dfcf8672e34b6db4/gds_py/gds_py_pip.txt
 REM JupyterLab plugin's
 jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
 jupyter labextension install jupyter-leaflet --no-build
